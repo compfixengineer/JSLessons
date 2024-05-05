c4b60a1609e46e5b1bda2926407a196cfc407c42
compfixengineer <compfixengineer@gmail.com>
2024-05-05 20:46:01 +0400

    Lesson #2

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let screens;
-let title;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let screens = "Простые, Сложные, Интерактивные";
+let title = "Название проекта";
+let screenPrice = 100;
+let rollback = 47;
+let fullPrice = 3000;
+let adaptive = false;
 
-alert("Вывод сообщения с любым текстом");
-console.log("Вывод сообщения с любым текстом");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+     
+console.log("Стоимость верстки экранов " + screenPrice + " ₽");
+console.log("Стоимость разработки сайта " + fullPrice + " ₽");
+
+
+screens = screens.toLowerCase();
+screens = screens.split(',');
+console.log(screens);
+console.log("Процент отката посреднику за работу "+ fullPrice * (rollback/100)+ " ₽");
+
