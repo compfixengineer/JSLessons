55d6baee11bdda7092bb6cf70a18159bd1f7a0e5
compfixengineer <compfixengineer@gmail.com>
2024-05-05 19:57:31 +0400

    add: variable - screens, title, screenPrice, rollback, fullPrice, adaptive. Alert and log.

---
+++ script.js
@@ -0,0 +1,9 @@
+let screens;
+let title;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert("Вывод сообщения с любым текстом");
+console.log("Вывод сообщения с любым текстом");
