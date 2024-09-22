ee3448c5b64794dfa87d6b8144b64b30ed824943
compfixengineer <compfixengineer@gmail.com>
2024-09-22 21:17:26 +0400

    fix: btn and input

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
-const title = document.getElementsByTagName('h1')[0].textContent;
-// console.log(title[0].textContent);
-const handlerBtn = document.getElementsByClassName('handler_btn')
+const title = document.getElementsByTagName('h1')[0];
+const startBtn = document.getElementsByClassName('handler_btn')[0];
+const resetBtn = document.getElementsByClassName('handler_btn')[1];
 const screenBtn = document.querySelector('.screen-btn')
 const otherItemsPercent = document.querySelectorAll(".other-items.percent");
 const otherItemsNumber = document.querySelectorAll('div > .number') // или ".other-items.number"
-const inputRange = document.querySelector('.rollback input[type=range]');
 const rangeValue = document.querySelector('.rollback span'); // или span[class=range-value]
-const totalInput = document.getElementsByClassName('total-input');
+const inputRange = document.querySelector('.rollback input[type=range]');
+const inputTotal = document.getElementsByClassName('total-input')[0];
+const inputTotalCount = document.getElementsByClassName('total-input')[1];
+const inputTotalCountOther = document.getElementsByClassName('total-input')[2];
+const inputTotalFullCount = document.getElementsByClassName('total-input')[3];
 let screen = document.querySelectorAll('.screen');
 
 
 console.log(title);
-console.log(handlerBtn);
+console.log(startBtn);
+console.log(resetBtn);
 console.log(screenBtn);
 console.log(otherItemsPercent);
 console.log(otherItemsNumber);
-console.log(inputRange);
 console.log(rangeValue);
-for (let i = 0; i < totalInput.length; i++) {
-    console.log(totalInput[i]);
-}
+console.log(inputRange);
+console.log(inputTotal);
+console.log(inputTotalCount);
+console.log(inputTotalCountOther);
+console.log(inputTotalFullCount);
 console.log(screen);
 
 
