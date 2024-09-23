6a524b67ea4087d0547d3bf84182a22d81b57b1e
compfixengineer <compfixengineer@gmail.com>
2024-09-23 22:41:03 +0400

    Lesson 10: you dont know js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,43 @@
-const title = document.getElementsByTagName('h1')[0];
-const startBtn = document.getElementsByClassName('handler_btn')[0];
-const resetBtn = document.getElementsByClassName('handler_btn')[1];
-const screenBtn = document.querySelector('.screen-btn')
-const otherItemsPercent = document.querySelectorAll(".other-items.percent");
-const otherItemsNumber = document.querySelectorAll('div > .number') // или ".other-items.number"
-const rangeValue = document.querySelector('.rollback span'); // или span[class=range-value]
-const inputRange = document.querySelector('.rollback input[type=range]');
-const inputTotal = document.getElementsByClassName('total-input')[0];
-const inputTotalCount = document.getElementsByClassName('total-input')[1];
-const inputTotalCountOther = document.getElementsByClassName('total-input')[2];
-const inputTotalFullCount = document.getElementsByClassName('total-input')[3];
-let screen = document.querySelectorAll('.screen');
+const books = document.querySelectorAll('.books');
+const book = document.querySelectorAll('.book');
+const adv = document.querySelector('.adv');
+const bookTitle = document.querySelectorAll('.book h2');
+const bookUl = document.querySelectorAll('.book ul');
+const bookUlLi = document.querySelectorAll('.book ul > li');
+const newElemLi = document.createElement('li');
+const bgImage = document.querySelector('body');
 
+console.log(bgImage);
+console.log(books);
+console.log(book);
+console.log(adv);
+console.log(bookTitle);
+console.log(bookUl);
+console.log(bookUlLi);
 
-console.log(title);
-console.log(startBtn);
-console.log(resetBtn);
-console.log(screenBtn);
-console.log(otherItemsPercent);
-console.log(otherItemsNumber);
-console.log(rangeValue);
-console.log(inputRange);
-console.log(inputTotal);
-console.log(inputTotalCount);
-console.log(inputTotalCountOther);
-console.log(inputTotalFullCount);
-console.log(screen);
+adv.remove();
 
+books[0].prepend(book[1]);
+books[0].insertBefore(book[3],book[5]);
+books[0].insertBefore(book[2],book[3]);
+books[0].insertBefore(book[3],book[5]);
+books[0].insertBefore(book[2],book[5]);
+books[0].insertBefore(book[5],book[2]);
 
+bookTitle[4].innerHTML = 'Книга 3. this и Прототипы Объектов';
+bookTitle[4].style.color = 'darkkhaki';
+
+bookUlLi[4].before(bookUlLi[6]);
+bookUlLi[4].before(bookUlLi[8]);
+bookUlLi[10].before(bookUlLi[2]);
+bookUlLi[49].before(bookUlLi[55]);
+bookUlLi[52].before(bookUlLi[51]);
+bookUlLi[53].before(bookUlLi[51]);
+bookUlLi[52].append(bookUlLi[53]);
+bookUlLi[50].append(bookUlLi[48]);
+bookUlLi[51].append(bookUlLi[54]);
+
+newElemLi.textContent = 'Глава 8: За пределами ES6';
+bookUlLi[25].append(newElemLi);
+
+bgImage.style.backgroundImage = 'url("/image/you-dont-know-js.jpg")';
